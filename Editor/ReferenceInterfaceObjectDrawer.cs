c1d4b402b95f3171e8677dd04ae3a4a429b57a14
yellowpaper <ayellowpaper@gmail.com>
2021-06-16 19:09:56 +0200

    object field now properly highlights if the DragAndDrop objects can be dropped onto it. Also validates DragAndDrop now

--- Editor/ReferenceInterfaceObjectDrawer.cs
+++ Editor/ReferenceInterfaceObjectDrawer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEditor;
 using System;
 using UnityEngine.UIElements;
+using System.Linq;
 
 namespace Zelude.Editor
 {
@@ -12,16 +13,52 @@ namespace Zelude.Editor
 	[CustomPropertyDrawer(typeof(InterfaceObject<,>))]
 	public class ReferenceInterfaceObjectDrawer : PropertyDrawer
 	{
-		const string _fieldName = "_underlyingValue";
+		private const string _fieldName = "_underlyingValue";
 
+		private static GUIStyle _style;
 		private static bool _queuedOpening = false;
 
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
+			if (_style == null)
+			{
+				_style = new GUIStyle(EditorStyles.label);
+				var objectFieldStyle = EditorStyles.objectField;
+				_style.font = objectFieldStyle.font;
+				_style.fontSize = objectFieldStyle.fontSize;
+				_style.fontStyle = objectFieldStyle.fontStyle;
+				_style.alignment = TextAnchor.MiddleRight;
+			}
+
 			var prop = property.FindPropertyRelative(_fieldName);
 			GetObjectAndInterfaceType(out var objectType, out var interfaceType);
-			EditorGUI.ObjectField(position, prop, interfaceType, label);
+
+			EditorGUI.BeginChangeCheck();
+			var prevValue = prop.objectReferenceValue;
+
+			var prevEnabledState = GUI.enabled;
+			if (Event.current.type == EventType.DragUpdated && position.Contains(Event.current.mousePosition) && GUI.enabled && !CanAssign(DragAndDrop.objectReferences, objectType, interfaceType))
+				GUI.enabled = false;
+
+			EditorGUI.ObjectField(position, prop, objectType, label);
+
+			GUI.enabled = prevEnabledState;
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				var newVal = prop.objectReferenceValue;
+				if (!(newVal == null || CanAssign(newVal, objectType, interfaceType)))
+					prop.objectReferenceValue = prevValue;
+			}
+
 			var controlID = GUIUtility.GetControlID(FocusType.Passive) - 1;
+			if (Event.current.type == EventType.Repaint)
+			{
+				var displayString = $"({ObjectNames.NicifyVariableName(interfaceType.Name)})";
+				var interfaceLabelPosition = position;
+				interfaceLabelPosition.width -= 22;
+				_style.Draw(interfaceLabelPosition, new GUIContent(displayString), controlID, DragAndDrop.activeControlID == controlID, position.Contains(Event.current.mousePosition));
+			}
 			var currentObjectPickerID = EditorGUIUtility.GetObjectPickerControlID();
 			if (controlID == currentObjectPickerID && _queuedOpening == false)
 			{
@@ -46,12 +83,17 @@ namespace Zelude.Editor
 					sb.Append("t:" + type.FullName + " ");
 			}
 
-			var filter = new ObjectSelectorFilter(sb.ToString(), obj => interfaceType.IsAssignableFrom(obj.GetType()) && objectType.IsAssignableFrom(obj.GetType()));
+			var filter = new ObjectSelectorFilter(sb.ToString(), obj => CanAssign(obj, interfaceType, objectType));
 			ObjectSelectorWindow.Show(property, obj => { property.objectReferenceValue = obj; property.serializedObject.ApplyModifiedProperties(); }, (obj, success) => { if (success) property.objectReferenceValue = obj; }, filter);
 			ObjectSelectorWindow.Instance.position = win.position;
+			win.titleContent.text = win.titleContent.text + $" ({interfaceType.Name})";
 			ObjectSelectorWindow.Instance.titleContent = win.titleContent;
 			_queuedOpening = false;
 		}
+
+		private bool CanAssign(UnityEngine.Object[] objects, Type objectType, Type interfaceType) => objects.All(obj => CanAssign(obj, objectType, interfaceType));
+
+		private bool CanAssign(UnityEngine.Object obj, Type objectType, Type interfaceType) => interfaceType.IsAssignableFrom(obj.GetType()) && objectType.IsAssignableFrom(obj.GetType());
 
 		private void GetObjectAndInterfaceType(out Type objectType, out Type interfaceType)
 		{
@@ -60,8 +102,8 @@ namespace Zelude.Editor
 			if (genericType == typeof(InterfaceObject<,>))
 			{
 				var types = fieldType.GetGenericArguments();
-				objectType = types[0];
-				interfaceType = types[1];
+				interfaceType = types[0];
+				objectType = types[1];
 				return;
 			}
 			objectType = typeof(UnityEngine.Object);
